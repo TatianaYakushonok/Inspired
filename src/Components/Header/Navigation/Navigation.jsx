9474ee1d96f86b00cfd23b793afddc66b730b118
TatianaYakushonok <yakush2@yandex.ru>
2023-07-05 15:00:11 +0700

    feat: add header (Wed, 05 Jul 2023 15:00:07 +0700)

---
+++ src/Components/Header/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import Category from "./Category/Category";
+import Gender from "./Gender/Gender";
+
+const Navigation = () => {
+  return ( 
+    <nav>
+      <div className="container">
+        <Gender />
+        <Category />
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
