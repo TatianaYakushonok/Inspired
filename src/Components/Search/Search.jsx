dede6c3a9119909e7d034967fc8112ffb427ee5f
TatianaYakushonok <yakush2@yandex.ru>
2023-07-18 12:33:41 +0700

    feat: add search (Tue, 18 Jul 2023 12:33:39 +0700)

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,19 +1,55 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Container from '../Layout/Container/Container';
 import style from './Search.module.scss';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { toggleSearch } from '../../features/searchSlice';
 
 const Search = () => {
 
   const { openSearch } = useSelector(state => state.search);
+  const initialValues = {
+    search: '',
+  }
+
+  const validationSchema = Yup.object({
+    search: Yup.string().required('Поле обязательно для заполнения'),
+  })
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSubmit = ({ search }, { resetForm }) => {
+    if (search.trim()) {
+      navigate(`/search?q=${search}`);
+      resetForm();
+      dispatch(toggleSearch(search));
+    }
+  }
 
   return ( 
     openSearch && 
     <div className={style.serach}>
       <Container>
-        <form className={style.form}>
-          <input className={style.input} type='search' name='search' placeholder='Найти...' />
-          <button className={style.btn} type='submit'>Искать</button>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form className={style.form}>
+            <Field 
+              className={style.input} 
+              type='search' 
+              name='search' 
+              placeholder='Найти...'
+            />
+
+            <ErrorMessage className={style.error} name='search' component={'p'} />
+
+            <button className={style.btn} type='submit'>Искать</button>
+          </Form>
+        </Formik>
       </Container>
     </div>
   );
--- src/SearchPage/SearchPage.jsx
+++ src/SearchPage/SearchPage.jsx
@@ -2,18 +2,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import style from './SearchPage.module.scss';
 import { useEffect } from 'react';
 import { fetchAll } from '../features/goodsSlice';
+import { useSearchParams } from 'react-router-dom';
+import Goods from '../Components/Goods/Goods';
 
 const SearchPage = () => {
 
   const dispatch = useDispatch();
   const { goodsList } = useSelector(state => state.goods);
+  let [ searchParams ] = useSearchParams();
 
   useEffect(() => {
-    dispatch(fetchAll({ q: goodsList}))
-  }, [goodsList, dispatch]);
+    const search = searchParams.get('q');
+    const params = { search };
+    dispatch(fetchAll(params));
+  }, [searchParams, dispatch]);
   
   return ( 
-    <></>
+    goodsList.length ?
+    <Goods title='Результат поиска' /> 
+    :
+    <h3 className={style.empty}>Ничего не найдено по вашему запросу</h3>
   );
 }
 
